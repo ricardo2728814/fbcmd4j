d58524e168bb031f1724ce5fb38c0f835e3c2e2a
Ricardo Romero Flores <al02728814@tecmilenio.mx>
2018-09-13 20:50:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # fbcmd4j
+
+## Installation
+```
+git clone https://github.com/ricardo2728814/fbcmd4j.git
+```
+## Usage
+**<TBD>**
+## Credits
+**<TBD>**
+## License
+[MIT](./LICENSE).
+
+:octocat:
---
+++ src/fbcmd4j/Fbcmd4j.java
@@ -0,0 +1,11 @@
+package fbcmd4j;
+/**
+ *
+ * @author Ricardo Romero
+ */
+public class Fbcmd4j {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
